1f03a5a4fc20849bae89ecd19ed8c8bb191cf7b5
Narek Okroyan <narek.okroyan@outlook.com>
2021-06-16 22:06:29 +0400

    added task pic functionality

--- src/models/task.js
+++ src/models/task.js
@@ -14,10 +14,21 @@ const taskSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         required: true,
         ref: 'User'
+    },
+    picture: {
+        type: Buffer
     }
 }, {
     timestamps: true
 })
+taskSchema.methods.toJSON = function() {
+    const task = this
+    const taskObject = task.toObject()
+
+    delete taskObject.picture
+
+    return taskObject
+}
 
 const Task = mongoose.model('Task', taskSchema)
 
--- src/routers/task.js
+++ src/routers/task.js
@@ -2,13 +2,26 @@ const express = require('express')
 const Task = require('../models/task')
 const auth = require('../middleware/auth')
 const router = new express.Router()
+const multer = require('multer')
+const sharp = require('sharp')
+
+const upload = multer({
+    limits: {
+        fileSize: 10000000
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(png|jpg|jpeg)/)) {
+            return cb(new Error('File must be an image'))
+        }
+        cb(undefined, true)
+    }
+})
 
 router.post('/tasks', auth, async(req, res) => {
     const task = new Task({
         ...req.body,
         owner: req.user._id
     })
-
     try {
         await task.save()
         res.status(201).send(task)
@@ -85,6 +98,38 @@ router.patch('/tasks/:id', auth, async(req, res) => {
         res.send(task)
     } catch (e) {
         res.status(400).send(e)
+    }
+})
+
+router.post('/tasks/:id/pic', auth, upload.single('pic'), async(req, res) => {
+    const buffer = await sharp(req.file.buffer).png().toBuffer()
+
+    const task = await Task.findById(req.params.id)
+
+    if (!task) {
+        return res.status(404).send()
+    }
+
+    task.picture = buffer
+    await task.save()
+    res.send(task)
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message })
+})
+
+router.get('/tasks/:id/pic', auth, async(req, res) => {
+    try {
+        const task = await Task.findById(req.params.id)
+
+        if (!task.picture) {
+            throw new Error('No picture')
+        }
+
+        res.set('Content-Type', 'image/png')
+        res.send(task.picture)
+    } catch (e) {
+        console.log(e)
+        res.status(404).send(e)
     }
 })
 
